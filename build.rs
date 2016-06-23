d73b2f9eb3e24ab27d02f7a1bf647dc5b7693869
Luminarys <postmaster@gensok.io>
2016-06-22 20:55:21 -0500

    Add appveyor info

--- build.rs
+++ build.rs
@@ -2,20 +2,29 @@ extern crate flate2;
 extern crate tar;
 
 use std::process::Command;
+use std::env;
+use std::fs::rename;
 use flate2::read::GzDecoder;
 use tar::Archive;
-use std::env;
 
 fn main() {
     let target = env::var("TARGET").unwrap(); 
-    if target == "x86_64-pc-windows-msvc" || target == "i686-pc-windows-msvc" {
-        let output = Command::new("curl").arg("-s").arg("-L").arg("https://github.com/Luminarys/Nishikaku-Bin/archive/0.1.0.tar.gz")
-            .output()
-            .unwrap_or_else(|e| { panic!("failed to execute process: {}", e) });
-
-        let resp = output.stdout;
-        let d = GzDecoder::new(&resp[..]).unwrap();
-        let mut a = Archive::new(d);
-        a.unpack("./target/").unwrap();
+    if target == "x86_64-pc-windows-msvc"{
+        download_files();
+        rename("target\\Nishikaku-Bin-0.1.0\\x86_64\\dll", "target\\win_rel").unwrap();
+    } else if target == "i686-pc-windows-msvc" {
+        download_files();
+        rename("target\\Nishikaku-Bin-0.1.0\\i686\\dll", "target\\win_rel").unwrap();
     }
+}
+
+fn download_files() {
+    let output = Command::new("curl").arg("-s").arg("-L").arg("https://github.com/Luminarys/Nishikaku-Bin/archive/0.1.0.tar.gz")
+       .output()
+       .unwrap_or_else(|e| { panic!("failed to execute process: {}", e) });
+
+    let resp = output.stdout;
+    let d = GzDecoder::new(&resp[..]).unwrap();
+    let mut a = Archive::new(d);
+    a.unpack("./target/").unwrap();
 }
